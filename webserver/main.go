9c003371a86f1d0705c18235d46bb8b4e70fcaea
usdj <denggerry@163.com>
2019-07-21 21:56:17 +0800

    update

--- webserver/main.go
+++ webserver/main.go
@@ -9,7 +9,6 @@ func firstPage(w http.ResponseWriter, r *http.Request){
 	io.WriteString(w, "<h1>Hello World</h1>")
 }
 
-
 func main() {
 	http.HandleFunc("/", firstPage)
 	http.ListenAndServe(":8000", nil)
