e7db2609f3ff6c2ac074f63024670b282727a901
usdj <denggerry@163.com>
2019-07-21 20:01:55 +0800

    update

--- deployserver/main.go
+++ deployserver/main.go
@@ -24,5 +24,5 @@ func firstPage(w http.ResponseWriter, r *http.Request){
 
 func main() {
 	http.HandleFunc("/", firstPage)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":5000", nil)
 }
